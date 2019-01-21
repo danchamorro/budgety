570d22ac2444d2bab09588f16c8e0328b86e8b68
Dan Chamorro <dan@danchamorro.com>
2019-01-21 17:16:24 -0500

    Budget Controller

---
+++ app.js
@@ -0,0 +1,14 @@
+let budgetController = (function() {
+  let x = 23;
+  let add = function(a) {
+    return x + a;
+  };
+
+  return {
+    publicTest: function(b) {
+      console.log(add(b));
+    }
+  };
+})();
+
+budgetController.publicTest(5);
